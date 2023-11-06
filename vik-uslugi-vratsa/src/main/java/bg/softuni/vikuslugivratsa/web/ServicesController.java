05b69dcc90deab245103acac48183d29dab0f765
Denislav Tsenov <106173870+dtsenov@users.noreply.github.com>
2023-11-06 12:15:39 +0200

    vik-uslugi-vratsa
    Adding some templates for services

---
+++ vik-uslugi-vratsa/src/main/java/bg/softuni/vikuslugivratsa/web/ServicesController.java
@@ -0,0 +1,40 @@
+package bg.softuni.vikuslugivratsa.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/services")
+public class ServicesController {
+
+    @GetMapping("/water-and-plumbing-installations")
+    public String waterAndPlumbingInstallations() {
+        return "water-and-plumbing-installations";
+    }
+
+    @GetMapping("/leaks")
+    public String leaks() {
+        return "leaks";
+    }
+
+    @GetMapping("/bathroom-leak-repair")
+    public String bathroomLeakRepair() {
+        return "bathroom-leak-repair";
+    }
+
+    @GetMapping("/toilet-cistern-repair")
+    public String toiletCisternRepair() {
+        return "toilet-cistern-repair";
+    }
+
+    @GetMapping("/faucet-replacement")
+    public String faucetReplacement() {
+        return "faucet-replacement";
+    }
+
+    @GetMapping("/water-meter-replacement")
+    public String waterMeterReplacement() {
+        return "water-meter-replacement";
+    }
+}
