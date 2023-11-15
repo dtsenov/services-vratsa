08c0bb76c6799c4dc323b7a367ffeb49a62c5b64
Denislav Tsenov <106173870+dtsenov@users.noreply.github.com>
2023-11-15 16:37:37 +0200

    services-vratsa
    add to cart js

---
+++ services-vratsa/src/main/resources/static/js/app.js
@@ -0,0 +1,37 @@
+// function attachEvents() {
+//
+// // let BASE_URL = ""
+// let addToCartBtn = document.getElementById("add-to-cart-btn");
+// let name = document.getElementById("name");
+// let price = document.getElementById("price");
+
+let totalCartPrice = 0;
+
+function addToCart(name, price) {
+    const cartItemsElement = document.getElementById("cartItems");
+    const totalPriceElement = document.getElementById("totalPrice");
+
+    const cartItemElement = document.createElement("div");
+    cartItemElement.className = 'cart-item';
+
+    const itemNameElement = document.createElement('span');
+    itemNameElement.textContent = name + price.toFixed(2) + ' лв.';
+
+    const deleteBtnElement = document.createElement('button');
+    deleteBtnElement.textContent = 'Изтрий';
+    deleteBtnElement.className = 'delete-btn';
+    deleteBtnElement.onclick = function () {
+        cartItemsElement.removeChild(cartItemElement);
+        totalCartPrice -= price;
+        totalPriceElement.textContent = totalCartPrice.toFixed(2);
+    }
+
+    cartItemElement.appendChild(itemNameElement);
+    cartItemElement.appendChild(deleteBtnElement);
+
+    cartItemsElement.appendChild(cartItemElement);
+
+    totalCartPrice += price;
+    totalPriceElement.textContent = totalCartPrice.toFixed(2);
+
+}
