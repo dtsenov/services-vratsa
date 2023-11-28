0927e13559794440e063fdd53ce7027e8401a5e2
Denislav Tsenov <106173870+dtsenov@users.noreply.github.com>
2023-11-28 15:14:47 +0200

    services-vratsa
    add bad request error handler

--- services-vratsa/src/main/java/bg/softuni/servicesvratsa/web/GlobalExceptionHandler.java
+++ services-vratsa/src/main/java/bg/softuni/servicesvratsa/web/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -24,11 +26,16 @@ public class GlobalExceptionHandler {
         return "404";
     }
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleInternalServerError() {
 
         return "500";
     }
 
+    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleBadRequestException() {
+        return "400";
+    }
 }
