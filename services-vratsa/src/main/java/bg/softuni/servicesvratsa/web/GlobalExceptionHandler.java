2b0306073195e3d5ad77da8cedc4350846e8f8c2
Denislav Tsenov <106173870+dtsenov@users.noreply.github.com>
2023-11-28 14:31:05 +0200

    services-vratsa
    add internal server error handler

--- services-vratsa/src/main/java/bg/softuni/servicesvratsa/web/GlobalExceptionHandler.java
+++ services-vratsa/src/main/java/bg/softuni/servicesvratsa/web/GlobalExceptionHandler.java
@@ -1,17 +1,34 @@
 package bg.softuni.servicesvratsa.web;
 
 import bg.softuni.servicesvratsa.exception.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ProductNotFoundException.class)
     public ModelAndView handleProductNotFoundException(ProductNotFoundException ex) {
-        ModelAndView modelAndView = new ModelAndView("error/error-404");
+        ModelAndView modelAndView = new ModelAndView("error/product-not-found");
         modelAndView.addObject("error", ex.getMessage());
         return modelAndView;
     }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFoundException() {
+        return "404";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleInternalServerError() {
+
+        return "500";
+    }
+
 }
