af40ffb08a87e764a75b82ca1147af333058ec7e
Denislav Tsenov <cenov.denislav@gmail.com>
2023-11-30 21:15:55 +0200

    services-vratsa
    adding more tests

--- services-vratsa/src/test/java/bg/softuni/servicesvratsa/service/impl/CartServiceTest.java
+++ services-vratsa/src/test/java/bg/softuni/servicesvratsa/service/impl/CartServiceTest.java
@@ -1,13 +1,18 @@
 package bg.softuni.servicesvratsa.service.impl;
 
+import bg.softuni.servicesvratsa.exception.ProductNotFoundException;
+import bg.softuni.servicesvratsa.model.binding.AddToCartDTO;
 import bg.softuni.servicesvratsa.model.entity.CartEntity;
+import bg.softuni.servicesvratsa.model.entity.UserEntity;
 import bg.softuni.servicesvratsa.model.view.CartViewModel;
+import bg.softuni.servicesvratsa.model.view.ProductCurrentViewModel;
 import bg.softuni.servicesvratsa.repository.CartRepository;
 import bg.softuni.servicesvratsa.service.CartService;
 import bg.softuni.servicesvratsa.service.ProductService;
 import bg.softuni.servicesvratsa.service.ServiceService;
 import bg.softuni.servicesvratsa.service.UserService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -17,9 +22,11 @@ import org.modelmapper.ModelMapper;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CartServiceTest {
@@ -30,17 +37,18 @@ public class CartServiceTest {
     private CartRepository mockCartRepository;
 
     @Mock
-    private  UserService mockUserService;
+    private UserService mockUserService;
 
     @Mock
-    private  ProductService mockProductService;
+    private ProductService mockProductService;
 
     @Mock
-    private  ModelMapper mockModelMapper;
+    private ModelMapper mockModelMapper;
 
     @Mock
-    private  ServiceService mockServiceService;
+    private ServiceService mockServiceService;
 
+    @BeforeEach
     void setUp() {
         serviceToTest = new CartServiceImpl(mockCartRepository,
                 mockUserService, mockProductService, mockModelMapper, mockServiceService);
@@ -60,38 +68,97 @@ public class CartServiceTest {
     @Test
     void testNonEmptyCart() {
 
-        List<CartEntity> testCartEntities = cartEntities();
+        CartEntity cartEntity = createCartEntity();
+        List<CartEntity> cart = new ArrayList<>();
+        cart.add(cartEntity);
 
         when(mockCartRepository.findAll())
-                .thenReturn(testCartEntities);
+                .thenReturn(cart);
 
-        List<CartViewModel> findAllInCart = serviceToTest.findAllInCart();
+        List<CartViewModel> allInCart = serviceToTest.findAllInCart();
 
-        //TODO
+        Assertions.assertEquals(1, allInCart.size());
+        Assertions.assertFalse(allInCart.isEmpty());
     }
 
-    private List<CartEntity> cartEntities() {
-        List<CartEntity> cart = new ArrayList<>();
+    @Test
+    void testAddProductToCartProductNotFound() {
+        String username = "pesho";
+        AddToCartDTO addToCartDTO = new AddToCartDTO();
+        addToCartDTO.setProductId("nonexistentProductId");
 
-        CartEntity waterMeter = new CartEntity();
-        waterMeter.setName("Water meter");
-        waterMeter.setQuantity(2);
-        waterMeter.setProductId("dsadshkwqhdasda");
-        waterMeter.setPrice(BigDecimal.valueOf(12));
-        waterMeter.setUsername("gosho");
-        waterMeter.setId(1L);
+        when(mockProductService.findProductByProductId(anyString()))
+                .thenThrow(new ProductNotFoundException("Product not found"));
 
-        CartEntity waterTap = new CartEntity();
-        waterMeter.setName("Water tap");
-        waterMeter.setQuantity(3);
-        waterMeter.setProductId("dsaxczxczxc");
-        waterMeter.setPrice(BigDecimal.valueOf(11));
-        waterMeter.setUsername("tosho");
-        waterMeter.setId(2L);
+        Assertions.assertThrows(ProductNotFoundException.class, () -> {
+            serviceToTest.addProductToCart(username, addToCartDTO);
+        });
 
-        cart.add(waterMeter);
-        cart.add(waterTap);
+        verify(mockCartRepository, never()).save(any(CartEntity.class));
+    }
 
-        return cart;
+    @Test
+    void testAddProductToCartNewCart() {
+        String username = "pesho";
+        AddToCartDTO addToCartDTO = new AddToCartDTO();
+        addToCartDTO.setProductId("newProductId");
+
+        UserEntity user = new UserEntity();
+        user.setUsername(username);
+
+        when(mockCartRepository.findByProductId(anyString()))
+                .thenReturn(Optional.empty());
+
+        when(mockUserService.findByUsername(username))
+                .thenReturn(user);
+
+        ProductCurrentViewModel product = new ProductCurrentViewModel();
+        product.setName("pizza");
+        product.setPrice(BigDecimal.TEN);
+        product.setProductId("newProductId");
+
+        when(mockProductService.findProductByProductId(anyString()))
+                .thenReturn(product);
+
+        serviceToTest.addProductToCart(username, addToCartDTO);
+
+        verify(mockCartRepository, times(1)).save(any(CartEntity.class));
+    }
+
+    @Test
+    void testAddProductToCartExistingCart() {
+        String username = "pesho";
+        AddToCartDTO addToCartDTO = new AddToCartDTO();
+        addToCartDTO.setProductId("existingProductId");
+
+        CartEntity existingCartEntity = new CartEntity();
+        existingCartEntity.setProductId("existingProductId");
+
+        UserEntity user = new UserEntity();
+        user.setUsername(username);
+
+        when(mockCartRepository.findByProductId(anyString()))
+                .thenReturn(Optional.of(existingCartEntity));
+
+        when(mockUserService.findByUsername(username))
+                .thenReturn(user);
+
+        serviceToTest.addProductToCart(username, addToCartDTO);
+
+        verify(mockCartRepository, times(1)).save(existingCartEntity);
+    }
+
+
+    private CartEntity createCartEntity() {
+
+        CartEntity cartEntity = new CartEntity();
+        cartEntity.setName("Water meter");
+        cartEntity.setQuantity(2);
+        cartEntity.setProductId("dsadshkwqhdasda");
+        cartEntity.setPrice(BigDecimal.valueOf(12));
+        cartEntity.setUsername("gosho");
+        cartEntity.setId(1L);
+
+        return cartEntity;
     }
 }
